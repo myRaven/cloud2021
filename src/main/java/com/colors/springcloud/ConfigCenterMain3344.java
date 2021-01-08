9ce959397fba8c5b6a67f2a61644fdea4f28baca
msn <1138613916@qq.com>
2021-01-08 12:25:01 +0800

    update

---
+++ src/main/java/com/colors/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,11 @@
+package com.colors.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author colors
+ * @date 2021/1/8 12:17
+ */
+@SpringBootApplication
+public class ConfigCenterMain3344 {
+}
